499814cc98c42e5523c4d2e153867db344e42741
billyogendo <39695428+billyogendo@users.noreply.github.com>
2019-01-23 15:15:53 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+env.BUILD_NUMBER="BASTION_123"
+env.TICKET_NUMBER="1234"
+env.BUILD_ID="BASTION_1.2.3."
+env.TARGET_HOST="muinde@54.174.22.138"
+env.TARGET_PASSWD="12345"
+env.BUILD_DISPLAY_NAME="jen"
+env.DIR="/tmp/targetfolder"
+env.DB_URL="hello"
+env.DB_URL2="hello2"
